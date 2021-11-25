2ef404bc8aad1e0244096a077c09cb23a7852d99
NilsDerenthal <derenils@kkg-do.eu>
2021-11-25 08:51:16 +0100

    abc

--- src/my_project/view/input/ArrayInputReciever.java
+++ src/my_project/view/input/ArrayInputReciever.java
@@ -22,11 +22,11 @@ public class ArrayInputReciever extends InputReceiver{
             case KeyEvent.VK_LEFT -> programController.moveOnArray(-1, 0);
             case KeyEvent.VK_UP -> programController.moveOnArray(0, -1);
             case KeyEvent.VK_DOWN -> programController.moveOnArray(0, 1);
+
             case KeyEvent.VK_SPACE -> programController.changeArr();
 
             case KeyEvent.VK_D -> programController.del();
             case KeyEvent.VK_A -> programController.add();
-
         }
     }
 }
